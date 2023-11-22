02bc8e7cda5e9371aa8065fd4df741b059cca522
nabelly <anabellymontibeller@gmail.com>
2023-11-22 16:59:38 -0300

    frontend starting

--- Brasilenha/Frontend/brasilenha-app/src/app/app.routes.ts
+++ Brasilenha/Frontend/brasilenha-app/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { TotemComponent } from './totem/totem.component';
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: LoginComponent},
+    {path: "Totem", component: TotemComponent},
+];
