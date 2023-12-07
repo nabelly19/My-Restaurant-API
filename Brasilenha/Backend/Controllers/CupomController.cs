84c06024037d7730a42f08d72ab5777a93761ef2
nabelly <anabellymontibeller@gmail.com>
2023-12-07 00:52:56 -0300

    add cupomController

---
+++ Brasilenha/Backend/Controllers/CupomController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers;
+
+using DTO;
+using Model;
+using System.Security.Cryptography;
+using Trevisharp.Security.Jwt;
+using System.ComponentModel;
+
+[ApiController]
+[Route("cupom")]
+public class CupomController : ControllerBase
+{
+    [HttpPost("register")]
+    [EnableCors("DefaultPolicy")]
+    public async Task<IActionResult>  Criar(
+        [FromBody]CupomData cupom,
+        [FromServices]CupomService servico
+    )
+    {
+        var erros = new List<string>();
+        if(cupom is null)
+            erros.Add("O formulário deve ser preenchido");
+      
+        if(erros.Count > 0)
+            return BadRequest(erros)/
+
+        await servico.Criar(cupom);
+        return Ok();
+     
+    }
+
+    [HttpGet()]
+    [EnableCors("DefaultPolicy")]
+    public async Task<IActionResult> Pegar(
+        [FromServices]ICupomService serviceo
+    )
+    {
+        var c = await servico.Pegar();
+        var erros = new List<string>();
+        if (erros.Count > 0)
+            return BadRequest(erros);
+        return Ok(new{c});
+    }
+
+    [HttpPost("pegarCupom")]
+    [EnableCors("DefaultPolicy")]
+    public async Task<IActionResult> PegarPeloCodigo(
+        [FromServices]ICupomService servico
+    )
+    {
+        var c = await servico.PegarPeloCodigo(cupom);
+        if(c == null)
+            return Ok(0);
+
+        var valor = c.Valor;
+        var erros = new List<string>();
+
+        if(erros.Count > 0)
+            return BadRequest(erros);
+
+        return Ok(valor);
+    }
+}
